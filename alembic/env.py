015c479af60efdd1754ffa85da06cec98d3cad46
nico <nicobejarano13@gmail.com>
2026-09-22 13:31:06 -0400

    Run Alembic migrations with async drivers

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,9 @@
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from trading_copilot.config import settings
@@ -9,7 +12,7 @@ from trading_copilot.persistence.models import Base
 config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-url = settings.database_url.replace("+aiosqlite", "").replace("+asyncpg", "")
+url = settings.database_url
 config.set_main_option("sqlalchemy.url", url.replace("%", "%%"))
 target_metadata = Base.metadata
 
@@ -25,14 +28,23 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
-    connectable = engine_from_config(
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool
     )
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
-        with context.begin_transaction():
-            context.run_migrations()
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    asyncio.run(run_async_migrations())
 
 
 run_migrations_offline() if context.is_offline_mode() else run_migrations_online()
